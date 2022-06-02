7fe3768768c180b93450524103694f6969965444
abhishreepavan <72576507+abhishreepavan@users.noreply.github.com>
2022-06-02 09:16:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,7 +54,16 @@ resource "aws_route_table_association" "public-subnet-1-route-table-association"
   subnet_id           = aws_subnet.public-subnet-1.id
   route_table_id      = aws_route_table.public-route-table.id
 }
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               =  "${var.private-subnet-1-cidr}"
+  availability_zone        = "ap-south-1b"
+  map_public_ip_on_launch  = false
 
+  tags      = {
+    Name    = "Private Subnet"
+  }
+}
 
 resource "aws_security_group" "security_group" {
   name        = "security_group"
